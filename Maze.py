87774719f13b58cc5e68f354f4530b6ff81910c3
EthanOB <gameguy0308@gmail.com>
2019-11-09 17:00:34 -0600

    I did more stuff

--- Maze.py
+++ Maze.py
@@ -46,28 +46,30 @@ Player = Player(input(''.join(["what is player's name? >>> "])), [], [], 0, 20)
 
 #Player.Pos[0] is x
 #Player.Pos[1] is y
-# def go(direction):
-#     if direction = "west":
-#         Player.Pos[0] = Player.Pos[0]# + 1
-#def west:
-#    Player.Pos[0] = Player.Pos[0] + 1
+def go(direction):
+     if direction == "west":
+         Player.Pos[0] = Player.Pos[0] - 1
+
 #Main Program
 print ("This is an adventure game.\nThe goal of this game is to escape the room")
-option
-option = Question("Do you want to eat the cake 1 or look around the room 2 >", 2)
-
-if option == 1:
-    Death("You died because the cake was poisoned")
+option = Question("Do you want to play a multiroom game 1 or single roomed game 2.\n (note only the single room game is working at the movement)")
 if option == 2:
-    while Player.
-        print ("There is a fan, a toybox, a door, and a window.")
-        option = Question("Inspect fan 1, inspect toybox 2, inspect door 3, and inspect window 4 >", 4)
-        if option == 1:
-            if Fan == 0:
-                option = Question("The fan is old and is turned off\nThere is a light switch on wall\nTurn the switch on 1 or don't tough the switch 2 >", 2)
-                if Fan == 1:
-                    option = Question("The fan is old and is turned on\nThere is a light switch on wall\nTurn the switch off 1 or don't tough the switch 2 >", 2)
-                    if option == 1:
-                        if Fan ==0:
-                            Fan = 1
-                            print ("The fan tuned on and the fan slowly started to turn")
+    option = Question("Do you want to eat the cake 1 or look around the room 2 >", 2)
+
+    if option == 1:
+        Death("You died because the cake was poisoned")
+        if option == 2:
+            while Gameover == 
+            print ("There is a fan, a toybox, a door, and a window.")
+            option = Question("Inspect fan 1, inspect toybox 2, inspect door 3, and inspect window 4 >", 4)
+            if option == 1:
+                if Fan == 0:
+                    option = Question("The fan is old and is turned off\nThere is a light switch on wall\nTurn the switch on 1 or don't tough the switch 2 >", 2)
+                    if Fan == 1:
+                        option = Question("The fan is old and is turned on\nThere is a light switch on wall\nTurn the switch off 1 or don't tough the switch 2 >", 2)
+                        if option == 1:
+                            if Fan ==0:
+                                Fan = 1
+                                print ("The fan tuned on and the fan slowly started to turn")
+if option == 1:
+    return "Error"
